52db4eb23cb9ed54a4759fec42642ed79c4180b5
Grzegorz Rogowski <rogowski.g@gmail.com>
2019-01-28 13:01:02 +0100

    added PlanRepository with methods for counting plans and getting last plan

---
+++ src/main/java/pl/coderslab/repository/PlanRepository.java
@@ -0,0 +1,11 @@
+package pl.coderslab.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.model.Plan;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    long countByAdmin(Admin admin); //do przetestowania czy zadziała jak już dodamy Admina
+    Plan findFirstByAdminOrderByCreatedDesc(Admin admin); //jw
+
+}
