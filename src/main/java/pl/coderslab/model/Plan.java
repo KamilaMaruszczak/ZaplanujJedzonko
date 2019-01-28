10d7441c528147153821de1c0e9c75caed671bdd
Grzegorz Rogowski <rogowski.g@gmail.com>
2019-01-28 12:28:47 +0100

    created entity Plan

---
+++ src/main/java/pl/coderslab/model/Plan.java
@@ -0,0 +1,58 @@
+package pl.coderslab.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 45)
+    private String name;
+
+    private String description;
+
+    private LocalDateTime created;
+
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private Admin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+}
