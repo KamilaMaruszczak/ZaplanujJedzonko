1e6633e0fc92ec0c1b17e4805900f72c2c45d4d3
KamilaMaruszczak <kamila.maruszczak@gmail.com>
2019-01-28 21:54:43 +0100

    Admin Entity

---
+++ src/main/java/pl/coderslab/model/Admin.java
@@ -0,0 +1,101 @@
+package pl.coderslab.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private int superadmin;
+    private int enabled;
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", superadmin=" + superadmin +
+                ", enabled=" + enabled +
+                '}';
+    }
+
+    public Admin() {
+    }
+
+    public Admin(String firstname, String lastname, String email, String password, int superadmin) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.superadmin = superadmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSuperadmin() {
+        return superadmin;
+    }
+
+    public void setSuperadmin(int superadmin) {
+        this.superadmin = superadmin;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+}
+
