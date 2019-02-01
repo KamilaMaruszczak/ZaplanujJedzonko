638b80c038564b7523ea275f10730433e8054fb3
piotrdabrowiecki <piotrdabrowiecki@gmail.com>
2019-02-01 11:00:07 +0100

    Merge branch 'feature-logowanie-i-rejestracja' of https://github.com/CodersLab/WAR_SJE_S_15_ScrumLab into feature/Recipe_form_and_controller
    # Conflicts:
    #	src/main/java/pl/coderslab/model/Plan.java
    #	src/main/java/pl/coderslab/model/Recipe.java
    #	src/main/webapp/WEB-INF/views/home.jsp

---
+++ src/main/resources/META-INF/weekdays.sql
@@ -0,0 +1,8 @@
+INSERT INTO `day_name` (`id`, `name`, `day_order`) VALUES
+(1, 'poniedziałek', 1),
+(2, 'wtorek', 2),
+(3, 'środa', 3),
+(4, 'czwartek', 4),
+(5, 'piątek', 5),
+(6, 'sobota', 6),
+(7, 'niedziela', 7);
